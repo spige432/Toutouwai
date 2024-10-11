a29d009acf66bad0d44cd63fd07b409c32bf4e7d
spige432 <spickaproffit@gmail.com>
2024-10-11 16:15:00 +1300

    Update PCA.R

--- PCA.R
+++ PCA.R
@@ -1,6 +1,6 @@
 library("pcadapt")  
 data <- read.pcadapt("out.recode.vcf", type = "vcf") 
-metadata <-read.table("metadata.txt",h=F)
+metadata <-read.table("../metadata.txt",h=T)
 
 
 x <- pcadapt(input = data, K = 20) 
@@ -10,7 +10,10 @@ plot(x, option = "screeplot")
 plot(x$scores[,1],x$scores[,2],col="white")
 text(x$scores[,1],x$scores[,2],metadata[,1])
 
-total<-cbind(x$scores[,1],x$scores[,2])
-colnames(total)<-c("PC1", "PC2")
-ggplot(total, aes(PC1, PC2)) + # Plotting with ggplot2 package geom_point() +theme_minimal() write.table(total,"PCA_dotterels.txt",quote=F,row.names=F,sep="\t") ggsave("PCA.pdf")
+total<-as.data.frame(cbind(x$scores[,1],x$scores[,2],metadata[,2]))
+total[,1]<-as.numeric(total[,1])
+total[,2]<-as.numeric(total[,2])
+
+colnames(total)<-c("PC1", "PC2","pop")
+ggplot(total, aes(PC1, PC2,col=pop)) + geom_point()# Plotting with ggplot2 package geom_point() +theme_minimal() write.table(total,"PCA_dotterels.txt",quote=F,row.names=F,sep="\t") ggsave("PCA.pdf")
 
