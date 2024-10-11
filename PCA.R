d8d5ee491616c3fa85cd5e3b2f2a01aa36395842
Ludovic Dutoit <dutoit.ludovic@gmail.com>
2024-10-11 15:47:06 +1300

    Create PCA.R

---
+++ PCA.R
@@ -0,0 +1,16 @@
+library("pcadapt")  
+data <- read.pcadapt("out.recode.vcf", type = "vcf") 
+metadata <-read.table("metadata.txt",h=F)
+
+
+x <- pcadapt(input = data, K = 20) 
+rownames(x$scores)<-metadata[,1]
+colnames(x$scores)<-paste("PC",1:20,sep="")
+plot(x, option = "screeplot")
+plot(x$scores[,1],x$scores[,2],col="white")
+text(x$scores[,1],x$scores[,2],metadata[,1])
+
+total<-cbind(x$scores[,1],x$scores[,2])
+colnames(total)<-c("PC1", "PC2")
+ggplot(total, aes(PC1, PC2)) + # Plotting with ggplot2 package geom_point() +theme_minimal() write.table(total,"PCA_dotterels.txt",quote=F,row.names=F,sep="\t") ggsave("PCA.pdf")
+
