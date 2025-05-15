01d5b687396164813b33998aadb5b0a4a1e7ba9b
spige432 <spickaproffit@gmail.com>
2025-05-15 15:53:56 +1200

    Update PCAreal.R

--- PCAreal.R
+++ PCAreal.R
@@ -38,3 +38,28 @@ PCA <- ggplot(eigenvec, aes(x = V3, y = V4, color = Population)) +
   ylab(paste0("PC2: ", round(percent_var[2], 2), "% variance"))
 
 print(PCA)
+
+
+#Show close up of crowded clusters (All populations aside from Hauturu and Kapiti)
+eigenvec_filtered <-  eigenvec[eigenvec$Population %in% c("Bushy Park", "Mangatutu", "Pureora", "Rotokare", "Tawharanui", "Tiritiri Matangi"),]
+filteredPCA <- ggplot(eigenvec_filtered, aes(x = V3, y = V4, color = Population)) +
+  geom_point(size = 2) +
+  xlab(paste0("PC1: ", round(percent_var[1], 2), "% variance")) +
+  ylab(paste0("PC2: ", round(percent_var[2], 2), "% variance"))
+
+print(filteredPCA)
+
+#MA13596 and MA13595 are within the TA/TM cluster (in TA cluster on tree)
+#those are the first two MA samples, but taken near other bands of the same numbers 
+#TM11694 is all alone (clusters together with TM in tree); 
+#TM934 and TM993 are within the BP, MA, PU, RO cluster. These band numbers are different than all the others, taken at a different time?
+#BP and RO extremely high crossover
+
+eigenvec_filtered2 <-  eigenvec[eigenvec$Population %in% c("Bushy Park",  "Pureora", "Rotokare"),]
+filteredPCA2 <- ggplot(eigenvec_filtered2, aes(x = V3, y = V4, color = Population)) +
+  geom_point(size = 2) +
+  xlab(paste0("PC1: ", round(percent_var[1], 2), "% variance")) +
+  ylab(paste0("PC2: ", round(percent_var[2], 2), "% variance"))
+
+print(filteredPCA2)
+
